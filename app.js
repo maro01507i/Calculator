746dde4bc48c72816dc07d42f14c7b73d7feb531
maro01507i <ivan.martin@opendeusto.es>
2017-10-25 17:33:56 +0200

    Added mongodb modules to the project
    Signed-off-by: maro01507i <ivan.martin@opendeusto.es>

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 var path = require('path');
-
+    mongo = require('mongodb').MongoClient;
     express = require('express');
     http=require('http');
 var app  = express();
+var url = 'mongodb://localhost:27017/calculador';
 
 app.get('/',function(req,res){
 	
-app.use(express.static(path.join(__dirname, '/resources')));
+app.use(express.static(path.join(__dirname, '/public')));
 	
-res.sendFile(__dirname + '/resources/index.html');
+res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/calculador',function(req,res){
-	
-app.use(express.static(path.join(__dirname, '/resources')));
-	
-res.sendFile(__dirname + '/resources/calculador.html');
+app.get('/get-data', function(req, res, next) {
+    var resultArray = [];
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        var cursor = db.collection('user-data').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function() {
+            db.close();
+            res.render('calculador', {items: resultArray});
+        });
+    });
 });
+
+app.get('/insertOP', function(req, res) {
+    console.log("Insertar");
+    var item = {
+        op1: req.body.op1,
+        lista:req.body.lista,
+        op2: req.body.op2,
+        result: req.body.result
+    };
+
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        db.collection('user-operations').insertOne(item, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item inserted');
+            db.close();
+        });
+    });
+
+    res.redirect('/');
+});
+
 
 var port = process.env.PORT || 8080;
 var server = app.listen(port);

a0d0e0f7acd6d4540a70b08fca01989859a320a6
maro01507i <ivan.martin@opendeusto.es>
2017-11-01 17:13:03 +0100

    Fixed error with database access
    Signed-off-by: maro01507i <ivan.martin@opendeusto.es>

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var path = require('path');
     mongo = require('mongodb').MongoClient;
     express = require('express');
     http=require('http');
+    bodyParser = require('body-parser');
 var app  = express();
-var url = 'mongodb://localhost:27017/calculador';
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/Calculador';
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
 	
@@ -12,43 +20,53 @@ app.use(express.static(path.join(__dirname, '/public')));
 res.sendFile(__dirname + '/public/index.html');
 });
 
+
 app.get('/get-data', function(req, res, next) {
     var resultArray = [];
     mongo.connect(url, function(err, db) {
         assert.equal(null, err);
-        var cursor = db.collection('user-data').find();
+        var cursor = db.collection('Calculador').find().sort({$natural:-1}).limit(4);
         cursor.forEach(function(doc, err) {
             assert.equal(null, err);
             resultArray.push(doc);
         }, function() {
             db.close();
-            res.render('calculador', {items: resultArray});
+            res.render('Calculador', {items: resultArray});
         });
     });
 });
 
-app.get('/insertOP', function(req, res) {
-    console.log("Insertar");
-    var item = {
+app.post('/insertOP', function(req, res) {
+
+   var item = {
         op1: req.body.op1,
         lista:req.body.lista,
         op2: req.body.op2,
-        result: req.body.result
+        result: calcular(req.body.op1,req.body.op2,req.body.lista)
     };
 
     mongo.connect(url, function(err, db) {
         assert.equal(null, err);
-        db.collection('user-operations').insertOne(item, function(err, result) {
+        db.collection('Calculador').insertOne(item, function(err, result) {
             assert.equal(null, err);
             console.log('Item inserted');
             db.close();
         });
     });
-
-    res.redirect('/');
+    app.use(express.static(path.join(__dirname, '/public')));
+    res.sendFile(__dirname + '/public/calculador.html');
 });
 
 
 var port = process.env.PORT || 8080;
 var server = app.listen(port);
 console.log('Express app started on port ' + port);
+
+function calcular(op1,op2,op){
+    if(op == "+")return Number(op1) + Number(op2);
+    else if(op == "-")return op1 - op2;
+    else if(op == "*")return op1 * op2;
+    else if(op == "/")return op1 / op2;
+    else if(op == "b")return Math.pow(op1,op2);
+
+}
---
+++ public/funtions.js
@@ -0,0 +1,125 @@
+function validarEnvio() {
+    if((!isNumber(document.getElementById("op1").value)) || (!isNumber(document.getElementById("op2").value))){
+        alert("Imposible leer los parámetros. Por favor, asegúrese de que ha introducido los operandos correctamente");
+        return false
+    }
+    else return true
+}
+
+
+function operar(){
+    if(!validarEnvio()){
+        alert("Imposible leer los parámetros. Por favor, asegúrese de que ha introducido los operandos correctamente");
+    }
+    else{
+        var op1 = parseInt(document.getElementById("op1").value);
+        var op2 = parseInt(document.getElementById("op2").value);
+
+        var result = calcular(op1,op2);
+        document.getElementById("result").value = result;
+    }
+}
+
+function calcular(op1,op2){
+    var op = document.getElementById("lista").value;
+    if(op == "+")return op1 + op2;
+    else if(op == "-")return op1 - op2;
+    else if(op == "*")return op1 * op2;
+    else if(op == "/")return op1 / op2;
+    else if(op == "b")return Math.pow(op1,op2);
+
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function isNumber2(n) {
+    if(!isNaN(parseFloat(n)) && isFinite(n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+function limpiarTexto() {
+    document.getElementById("op1").value ="";
+    document.getElementById("op2").value ="";
+    document.getElementById("result").value ="";
+}
+
+function copiado() {
+    alert("Correo copiado al portapapeles")
+}
+
+function validarMail(mail)
+{
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById("mail").value ))
+    {
+        return (true)
+    }
+    alert("Correo no válido.")
+    document.getElementById("mail").value == ""
+    return (false)
+}
+
+function comprobarContra() {
+    var contra1 = document.getElementById("pass1").value
+    var contra2 = document.getElementById("pass2").value
+    if(contra1.length == 0) {
+        alert("Contraseña vacía.")
+        return false
+    }
+    if(contra1.localeCompare(contra2) == 0){
+        return true
+    }
+    else{
+        alert("Las contraseñas no coinciden.")
+        document.getElementById("pass2").value == ""
+        return false
+    }
+
+}
+
+function comprobarTodo() {
+    if(document.getElementById('terminosCondiciones').checked){
+       var bol1 = validarMail()
+		var bol2 = comprobarContra()
+		if(bol1 && bol2 == true){
+            document.getElementById("registro").disabled = false
+			return true
+		}
+		else{
+            document.getElementById('terminosCondiciones').checked = false
+			return false
+		}
+
+    }
+}
+
+function deshabilitar() {
+    if(document.getElementById('terminosCondiciones').checked){
+        document.getElementById("registro").disabled = true
+        document.getElementById('terminosCondiciones').checked = false
+    }
+}
+
+function eliminarOp(pos) {
+
+}
+
+function equivalenciaOP(op) {
+    if(op == "+")return 1;
+    else if(op == "-")return 2;
+    else if(op == "*")return 3;
+    else if(op == "/")return 4;
+    else if(op == "b")return 5;
+}
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").toggle();
+        $("h1").toggle();
+    });
+});
